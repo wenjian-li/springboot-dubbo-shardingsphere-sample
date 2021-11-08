59f65308780dc32b8b0bfbc24aceeaa2a9605cf1
vincent.li <977286367@qq.com>
2021-11-08 09:00:00 +0800

    更新DateUtils

--- sample-common/src/main/java/com/sample/lwj/utils/DateUtils.java
+++ sample-common/src/main/java/com/sample/lwj/utils/DateUtils.java
@@ -1,6 +1,5 @@
 package com.sample.lwj.utils;
 
-import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
 import org.joda.time.LocalDate;
@@ -70,7 +69,7 @@ public class DateUtils {
      * @param pattern 日期的格式，如：DateUtils.DATE_TIME_PATTERN
      */
     public static Date stringToDate(String strDate, String pattern) {
-        if (StringUtils.isBlank(strDate)) {
+        if (strDate == null || "".equals(strDate.trim())) {
             return null;
         }
 
